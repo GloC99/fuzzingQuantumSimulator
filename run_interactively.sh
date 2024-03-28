d154b46ffcb4530960cfc198d2e6d8aba41b0dae
Dan Blackwell <danblackwell95@yahoo.co.uk>
2024-03-28 14:39:36 +0000

    Added run_interactively.sh script to quickly get into an interactive docker session

---
+++ run_interactively.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+docker build -t quant_fuzz .
+docker run -it quant_fuzz bash
