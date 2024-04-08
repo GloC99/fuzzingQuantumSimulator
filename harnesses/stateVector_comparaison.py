3e23efdc9a9dd0746a49e3825cbdd6bb1f93ab0b
Avner Bensoussan <k20013502@kcl.ac.uk>
2024-04-08 13:55:05 +0200

    Correct last commit, update file

--- harnesses/stateVector_comparaison.py
+++ harnesses/stateVector_comparaison.py
@@ -10,15 +10,3 @@ def state_vector_comparaison(qasm_string, dist):
     dist_stateVector = dict(zip(state_vector.to_dict().keys() ,state_vector.probabilities()))
 
     return get_kl_divergence(dist, dist_stateVector, False)
-
-
-qasm_string = '''OPENQASM 3.0;
-include "stdgates.inc";
-qubit[3] r1;
-h r1[0];
-ry(-23.22e+128) r1;'''
-
-
-counts = {'011': 33, '001': 177, '111': 153, '110': 1038, '010': 185, '000': 1070, '101': 1059, '100': 6285}
-
-print(state_vector_comparaison(qasm_string, counts))
